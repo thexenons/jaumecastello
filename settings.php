6cd1e58a6b3e24cc138a7018decee68a08faf0bb
thexenons <jaume97.cs@gmail.com>
2018-06-01 18:10:51 +0200

    Partial Update

--- settings.php
+++ settings.php
@@ -6,4 +6,15 @@ if (!isset($_GET['seccion']))
     header('Location: ?seccion=index.php');
 }
 
+$server_name = 'http://'.$_SERVER['SERVER_NAME'];
+$url_actual = $url_web.$_SERVER['PHP_SELF'];
+
 $seccion = $_GET['seccion'];
+
+/* MENÚ */
+$array_menu = [
+    [
+        'nombre' => 'Ejemplo',
+        'url' => '#'
+    ]
+];
--- tpl_header.php
+++ tpl_header.php
@@ -1,21 +1,30 @@
 <header>
-    <div id="main_header" class="col-12">
+    <div class="container display-lg-flex">
         <div id="mobile_menu">
-            <div id="mobile_menu_icon">
-                <i class="fa fa-bars"></i>
+            <div id="mobile_menu_icon" class="display-lg-none">
+                <div id="mobile_menu_open">
+                    <i class="fa fa-bars"></i>
+                </div>
+                <div id="mobile_menu_close">
+                    <i class="fa fa-close"></i>
+                </div>
             </div>
             <nav id="mobile_menu_nav">
-                
+                <?php foreach($array_menu as $item_menu): ?>
+                <a href="<?php echo $item_menu['url'] ?>"><?php echo $item_menu['nombre'] ?></a>
+                <?php endforeach; ?>
             </nav>
         </div>
-        <div class="logo">
-            <img src="img/logo.png" alt="">
+        <div id="logo">
+            <a href="<?php echo $server_name ?>">
+                <img src="img/logo.png" alt="">
+            </a>
         </div>
-    </div>
-    <div id="sub_header" class="display-none display-md-flex">
-        <div class="desktop_menu">
+        <div class="desktop_menu display-none display-lg-flex">
             <div class="container">
-        
+                <?php foreach($array_menu as $item_menu): ?>
+                <a href="<?php echo $item_menu['url'] ?>"><?php echo $item_menu['nombre'] ?></a>
+                <?php endforeach; ?>
             </div>
         </div>
     </div>
